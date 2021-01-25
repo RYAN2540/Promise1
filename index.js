e6efafdaf9acb6672a1a195c8156016a992b7623
RYAN2540 <62008223+RYAN2540@users.noreply.github.com>
2021-01-25 22:21:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,15 @@ const fetchData = async() => {
     }
 }
 fetchData()
+const countriesAPI = 'https://restcountries.eu/rest/v2/all'
+const countryData = async () => {
+    const response =  await fetch(countriesAPI);
+    const data = await response.json();
+    data.forEach(element => {
+        var languages = element.languages;
+        languages.forEach(language => {
+            console.log(`Country: ${element.name}, Language: ${language.name}, Population: ${element.population}, Area:${element.area}`);
+        })
+    });
+}
+countryData();
